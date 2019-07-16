e04e1ec60b94f96938ce5f11d33309efe0d8b7d7
Delwyn Yit <khmer.delwyn.9523@gmail.com>
2019-07-15 23:14:49 -0400

    fixed api

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
-// const geocodeAPI = require('../../CONSTANTS')
-const geocodeAPI = 'pk.eyJ1Ijoia2htZXJiMTAiLCJhIjoiY2p4emhzd2k2MDJhbTNscW55bngyazhmcSJ9.94Q92J4HfB2IRLXDtqhZ7w'
+const geocodeAPI = require('../../CONSTANTS')
+// const geocodeAPI =
 
 const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token='+geocodeAPI+'&limit=1'
